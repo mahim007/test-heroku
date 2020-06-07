accdf0609c7521f491857c2eb788a82f592438e7
Ashraful Islam Mahim <ashrafulmahim@gmail.com>
2020-06-07 21:10:48 +0600

    react build files added for client

---
+++ src/main/resources/static/precache-manifest.9ad9a36866f2f78dfb37a31fef737de2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2fd7ff535c7f5106aacc1d2465b57c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e676fc8c218094d6cad",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "022e5487332ea301b52d",
+    "url": "/static/css/main.86ded62d.chunk.css"
+  },
+  {
+    "revision": "8e676fc8c218094d6cad",
+    "url": "/static/js/2.b2283c64.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b2283c64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "022e5487332ea301b52d",
+    "url": "/static/js/main.abbaa981.chunk.js"
+  },
+  {
+    "revision": "5bab9fa149039e52cbed",
+    "url": "/static/js/runtime-main.751529da.js"
+  }
+]);
